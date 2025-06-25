751a585e04106b6dd33517e75729c855e0001b47
Mehmet Emre <maemre2@gmail.com>
2025-06-24 19:17:14 -0700

    switch to dynamic types

--- ParserCombinators/Memoized.lean
+++ ParserCombinators/Memoized.lean
@@ -7,51 +7,28 @@ import Std.Data.HashMap.AdditionalOperations
 
 variable { α : Type }  { μ : Type → Type }
 
-structure Tag (α : Type) where
+-- Tags for each parser, these don't carry type information.
+--
+-- NOTE(maemre): A better option would be to use pointers to the parser functions as keys,
+-- but I am not sure if that runs into issues with the termination checker.
+--
+-- If we want, we can store the type names here later.
+structure Tag where
   id : Int
-  deriving DecidableEq
-  -- TODO: Can we add the below change here for code cleanup?
+  deriving DecidableEq, BEq, Hashable
 
--- Is this necessary? I feel like DecidableEq does this, but maybe I'm misunderstanding what DecidableEq does?
--- I looked it up and it looks like we could just do this on line 13:
--- deriving DecidableEq, BEq, Hashable (we still need BEq to be added as an instance, but Lean can
--- supposedly do it for us??)
-instance : BEq (Tag α) where
-  beq t u := t.id == u.id
-
-instance : Hashable (Tag α) where
-  hash t := hash t.id
-
-def resultType (tag : Tag α) := α
-
--- TODO: derive hashable instances so we need it only for α
---
--- Also enforce some inequality constraint on carried types for tags?
--- Like if t : Tag α, u : Tag β then u == v → α = β
-
--- TODO: Would type erasure be a better solution?
--- Here's why I think it would be:
----- It's a simpler approach from a design perspective.
----- While it would lead to a higher risk of runtime failures in theory, I think we could easily
----- encapsulate the lack of safety into one, guaranteed-to-compile function for memoization
----- This function would be something we write with guarantees that if we're at a specific cache slot, we could
----- ONLY have previously executed on the exact same parser (and thus it would have the exact same type)
----- Does this mean that we have more responsibility? Yes, but we only have to have that responsibility in one
----- specific "unsafe" function, and we could build the rest of our library on that function safely (AND elegantly, IMO)
---
--- Also, the α should be pushed inside the table field as much as possible
-structure MemoData [Hashable (Tag Unit)] [BEq (Tag Unit)] where
+structure MemoData where
   nextTag : Int -- need only the data inside
-  table : Std.DHashMap (Tag Unit) (fun t => Std.HashMap Int (resultType t))
+  table : Std.HashMap Tag (Std.HashMap Int Dynamic)
 
-def startState { α : Type } [Hashable (Tag α)] : MemoData := {
+def startState : MemoData := {
   nextTag := 0
-  table := Std.DHashMap.empty
+  table := Std.HashMap.empty
 }
 
 abbrev Memo (β : Type) := StateM MemoData β
 
-def fresh : Memo (Tag α) := do
+def fresh : Memo Tag := do
   let memo <- get
   set { memo with nextTag := memo.nextTag + 1 }
   pure ⟨ memo.nextTag ⟩
@@ -59,7 +36,6 @@ def fresh : Memo (Tag α) := do
 -- Some of the α's here should become existential/hidden
 structure Parser (μ : Type -> Type) [Monad μ] (α : Type) where
   getState : Int -> (StateT MemoData) (ReaderM String) (Std.HashMap Int (μ α))
-
 
 instance [Monad μ] : Functor (Parser μ) where
   map f x := ⟨ Functor.map (Std.HashMap.map (fun _ => Functor.map f)) ∘ x.getState ⟩
