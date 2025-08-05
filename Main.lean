a1f82994e43a9715774533f7e8a912ee3ce5c584
Mehmet Emre <maemre2@gmail.com>
2025-08-05 14:15:39 -0700

    a driver for parser benchmarks

--- Main.lean
+++ Main.lean
@@ -1,7 +1,35 @@
 import ParserCombinators
 
+@[simp]
+def tag : PUnit → Type := fun _ => List Char
+
+instance : DecidableEq (tag t) := by
+  unfold tag
+  infer_instance
+
+-- rightRec ::= a | a rightRec
+def rightRec : ParserM (tag := tag) Char Const (List Char) :=
+  memoize Counter.empty (fun recur _ =>
+    let secondCase := liftA2 List.cons (terminal'  'a') (recur PUnit.unit)
+    (pure <$> terminal'  'a') ⊔ secondCase
+  ) PUnit.unit
+
+-- leftRec ::= a | leftRec a
+def leftRec : ParserM (tag := tag) Char Const (List Char) :=
+  memoize Counter.empty (fun recur _ =>
+    let secondCase := liftA2 List.concat (recur PUnit.unit) (terminal'  'a')
+    (pure <$> terminal'  'a') ⊔ secondCase
+  ) PUnit.unit
+
+def parsers := [("leftRec", leftRec), ("rightRec", rightRec)]
+
 def main (args : List String) : IO UInt32 := match args with
-  | [s] => do
-    IO.println $ (runParser'.{0} (μ := Const) concatParser s).1.toList
+  | [] => do
+    for (name, parser) in parsers do
+      IO.println $ "running " ++ name
+      for n in List.range 5 do
+        let len := 2 ^ n + 1
+        let input := Array.replicate len 'a'
+        let _ <- timeit len.repr $ pure $ (runParser parser input).1.toList
     pure 0
   | _ => pure 1
